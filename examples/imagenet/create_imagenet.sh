db6910c544cba47db5830583b034015263d244c2
pentiumx <pentiumx@live.jp>
2014-11-30 14:51:54 +0900

    Add custom files

--- examples/imagenet/create_imagenet.sh
+++ examples/imagenet/create_imagenet.sh
@@ -6,12 +6,12 @@ EXAMPLE=examples/imagenet
 DATA=data/ilsvrc12
 TOOLS=build/tools
 
-TRAIN_DATA_ROOT=/path/to/imagenet/train/
-VAL_DATA_ROOT=/path/to/imagenet/val/
+TRAIN_DATA_ROOT=/Users/pentiumx/Projects/caffe/data/ilsvrc12/
+VAL_DATA_ROOT=/Users/pentiumx/Projects/caffe/data/ilsvrc12/
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
 # already been resized using another tool.
-RESIZE=false
+RESIZE=true
 if $RESIZE; then
   RESIZE_HEIGHT=256
   RESIZE_WIDTH=256
